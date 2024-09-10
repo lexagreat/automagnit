c2535f97ce7fab3f3a8ec7c051c71d1ee3b4dffc
lexagreat <9608798010@mail.ru>
2024-09-10 20:04:57 +0300

    add second section

--- #src/js/app.js
+++ #src/js/app.js
@@ -4,14 +4,35 @@
       webP.onload = webP.onerror = function () {
          callback(webP.height == 2);
       };
-      webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+      webP.src =
+         "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
    }
    testWebP(function (support) {
-
       if (support == true) {
-         document.querySelector('html').classList.add('webp');
+         document.querySelector("html").classList.add("webp");
       } else {
-         document.querySelector('html').classList.add('no-webp');
+         document.querySelector("html").classList.add("no-webp");
       }
    });
-})()
+})();
+
+const youtubeSwiper = new Swiper(".youtube-section .swiper", {
+   slidesPerView: 1,
+   spaceBetween: 20,
+   mousewheel: {
+      enabled: true,
+      forceToAxis: true,
+   },
+   autoplay: {
+      enabled: true,
+      delay: 1500,
+   },
+   loop: true,
+   speed: 1000,
+   breakpoints: {
+      992: {
+         slidesPerView: 2,
+         spaceBetween: 42,
+      },
+   },
+});
